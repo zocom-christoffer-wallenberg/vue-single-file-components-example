3d4d6ece5370321931815aba2ca2708cb85c4c4d
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2020-02-12 10:09:19 +0100

    Example code

---
+++ app.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="wrapper">
+        <h1>{{ message }}</h1>
+        <CharacterName name="Hermione Granger" />
+    </div>
+</template>
+
+<script>
+import CharacterName from './character-name';
+
+export default {
+    components: {
+        CharacterName
+    },
+    data: () => {
+        return {
+            message: "Stop, stop stop! You're going to take someone's eye out. Besides, you're saying it wrong." +
+        " It's Levi-o-sa, not Levio-sar."
+        }
+    }
+}
+</script>
+
+<style>
+    body {
+        width: 100vw;
+        height: 100vh;
+        background: #FAF5EB;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .wrapper {
+        max-width: 1000px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    h1 {
+        color: #692D55;
+    }
+</style>
---
+++ character-name.vue
@@ -0,0 +1,30 @@
+<template>
+  <article>
+      <h2> - {{ name }}</h2>
+      <input type="text" v-model="text" placeholder="How said it?">
+      <p> {{ text }} </p>
+      <button v-on:click="answer">Answer</button>
+  </article>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String
+    },
+    data: () => {
+        return {
+            text: ''
+        }
+    },
+    methods: {
+        answer:function() {
+            alert(this.text);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
